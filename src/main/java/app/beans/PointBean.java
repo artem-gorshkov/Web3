943514dee90422d7150ce6ea1a1497776ee271f1
gorshik <47691597+gorshik@users.noreply.github.com>
2020-01-18 11:29:52 +0300

    Create README.md

--- src/main/java/app/beans/PointBean.java
+++ src/main/java/app/beans/PointBean.java
@@ -3,8 +3,8 @@ package app.beans;
 import java.math.BigDecimal;
 
 public interface PointBean {
-    static final String in ="В зоне";
-    static final String out ="Не в зоне";
+    String in ="В зоне";
+    String out ="Не в зоне";
     void addPoint();
     default String findResult(Double x, BigDecimal y, Double r) {
         double Y = Double.parseDouble(y.toString()); //for compare with x and r
